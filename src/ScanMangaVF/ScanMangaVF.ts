49ba1b0fb1acab5e3f5c703ab899b334c1a24441
Navalex <alexnava@hotmail.fr>
2023-09-16 18:33:32 +0200

    Switch to paperback v0.8

--- src/ScanMangaVF/ScanMangaVF.ts
+++ src/ScanMangaVF/ScanMangaVF.ts
@@ -1,23 +1,6 @@
-import {
-    Chapter,
-    ChapterDetails,
-    ContentRating,
-    HomeSection,
-    Manga,
-    PagedResults,
-    SearchRequest,
-    Request,
-    Response,
-    Source,
-    SourceInfo,
-    TagType,
-    RequestManagerInfo,
-} from "paperback-extensions-common";
-
+import { Chapter, ChapterDetails, ContentRating, HomeSection, SourceManga, PagedResults, SearchRequest, Request, Response, Source, SourceInfo, BadgeColor, RequestManagerInfo } from "@paperback/types";
 import { Parser } from "./parser";
-
 const SOURCE_DOMAIN = "https://scanmanga-vf.ws";
-
 export const ScanMangaVFInfo: SourceInfo = {
     version: "1.0.0",
     name: "ScanManga-VF",
@@ -30,21 +13,18 @@ export const ScanMangaVFInfo: SourceInfo = {
     sourceTags: [
         {
             text: "French",
-            type: TagType.GREY,
+            type: BadgeColor.GREY
         },
         {
             text: "Cloudflare",
-            type: TagType.RED,
+            type: BadgeColor.RED
         },
-    ],
+    ]
 };
-
-const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44";
-
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44";
 export class FlameScans extends Source {
     baseUrl = SOURCE_DOMAIN;
-    requestManager = createRequestManager({
+    requestManager = App.createRequestManager({
         requestsPerSecond: 3,
         requestTimeout: 8000,
         interceptor: {
@@ -53,119 +33,100 @@ export class FlameScans extends Source {
                     ...(request.headers ?? {}),
                     ...{
                         "user-agent": userAgent,
-                        referer: `${this.baseUrl}/`,
-                    },
+                        referer: `${this.baseUrl}/`
+                    }
                 };
-
                 return request;
             },
-
             interceptResponse: async (response: Response): Promise<Response> => {
                 return response;
-            },
-        },
+            }
+        }
     });
-
     RETRY = 5;
     parser = new Parser();
-
     override getMangaShareUrl(mangaId: string): string {
         return `${this.baseUrl}/manga/${mangaId}`;
     }
-
-    async getMangaDetails(mangaId: string): Promise<Manga> {
-        const request = createRequestObject({
+    async getMangaDetails(mangaId: string): Promise<SourceManga> {
+        const request = App.createRequest({
             url: `${this.baseUrl}/manga/${mangaId}`,
-            method: "GET",
+            method: "GET"
         });
         const response = await this.requestManager.schedule(request, this.RETRY);
         this.CloudFlareError(response.status);
         const $ = this.cheerio.load(response.data);
         return this.parser.parseMangaDetails($, mangaId);
     }
-
     async getChapters(mangaId: string): Promise<Chapter[]> {
-        const request = createRequestObject({
+        const request = App.createRequest({
             url: `${this.baseUrl}/manga/${mangaId}`,
-            method: "GET",
+            method: "GET"
         });
-
         const response = await this.requestManager.schedule(request, this.RETRY);
         this.CloudFlareError(response.status);
         const $ = this.cheerio.load(response.data);
         return this.parser.parseChapters($, mangaId, this);
     }
-
     async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
-        const request = createRequestObject({
+        const request = App.createRequest({
             url: `${chapterId}`,
-            method: "GET",
+            method: "GET"
         });
-
         const response = await this.requestManager.schedule(request, this.RETRY);
         this.CloudFlareError(response.status);
         const $ = this.cheerio.load(response.data);
         return this.parser.parseChapterDetails($, mangaId, chapterId);
     }
-
     async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
         let page = metadata?.page ?? 1;
-        if (page == -1) return createPagedResults({ results: [], metadata: { page: -1 } });
-
+        if (page == -1)
+            return App.createPagedResults({ results: [], metadata: { page: -1 } });
         const search = query.title?.replace(/ /g, "+").replace(/[’'´]/g, "%27") ?? "";
         const param = `filterList?page=${page}&tag=&alpha=${search}&sortBy=name&asc=true`;
-        const request = createRequestObject({
+        const request = App.createRequest({
             url: `${this.baseUrl}`,
             method: "GET",
-            param,
+            param
         });
-
         const data = await this.requestManager.schedule(request, this.RETRY);
         this.CloudFlareError(data.status);
         const $ = this.cheerio.load(data.data);
         const manga = this.parser.parseSearchResults($);
-
         page++;
-        if (manga.length < 10) page = -1;
-
-        return createPagedResults({
+        if (manga.length < 10)
+            page = -1;
+        return App.createPagedResults({
             results: manga,
-            metadata: { page: page },
+            metadata: { page: page }
         });
     }
-
     override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
-        const request = createRequestObject({
+        const request = App.createRequest({
             url: `${this.baseUrl}`,
-            method: "GET",
+            method: "GET"
         });
         const response = await this.requestManager.schedule(request, this.RETRY);
         this.CloudFlareError(response.status);
         const $ = this.cheerio.load(response.data);
-
         this.parser.parseHomeSections($, sectionCallback);
     }
-
     override getCloudflareBypassRequest(): Request {
-        return createRequestObject({
+        return App.createRequest({
             url: this.baseUrl,
             method: "GET",
             headers: {
                 "user-agent": userAgent,
-                referer: `${this.baseUrl}/`,
-            },
+                referer: `${this.baseUrl}/`
+            }
         });
     }
-
     CloudFlareError(status: any) {
         if (status == 503) {
-            throw new Error(
-                "CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > <The name of this source> and press Cloudflare Bypass"
-            );
+            throw new Error("CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > <The name of this source> and press Cloudflare Bypass");
         }
     }
 }
-
 export interface FSResponse extends Response {
     fixedData: string;
 }
